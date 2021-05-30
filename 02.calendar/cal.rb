1b4659fdc026c20728a665c0a9bf5ac19d921950
niikz <i.k307112@gmail.com>
2021-05-30 17:44:23 +0900

    カレンダーの装飾を追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,11 +2,15 @@
 
 require 'date'
 
-# 今月のカレンダーを表示
 year = 2021
 month = 5
 first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
+# カレンダーの装飾を追加
+str = "#{month}月 #{year}"
+puts str.center(20)
+puts "日 月 火 水 木 金 土"
+# 今月のカレンダーを表示
 (first_date.mday..last_date.mday).each do |i|
   date = Date.new(year, month, i)
   day = date.strftime('%e')

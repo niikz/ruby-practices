e4d3cc572b7d1013f14238dd29b55905792bf4b1
niikz <i.k307112@gmail.com>
2021-05-30 17:45:29 +0900

    最初の週に空白を挿入

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -14,6 +14,24 @@ puts "日 月 火 水 木 金 土"
 (first_date.mday..last_date.mday).each do |i|
   date = Date.new(year, month, i)
   day = date.strftime('%e')
+  white_space = "   "
+  if i == 1
+    day_of_the_week = first_date.cwday
+    case day_of_the_week
+    when 1
+      day = white_space + day
+    when 2
+      day = white_space * 2 + day
+    when 3
+      day = white_space * 3 + day
+    when 4
+      day = white_space * 4 + day
+    when 5
+      day = white_space * 5 + day
+    when 6
+      day = white_space * 6 + day
+    end
+  end
   if date.cwday % 6 == 0
     print(day, "\n")
   else

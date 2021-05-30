62e0b1293be74d1dd4e7ef30fdb7c979eb0cc340
niikz <i.k307112@gmail.com>
2021-05-30 18:11:52 +0900

    コマンド入力から年月を指定

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,16 +1,36 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
 require 'date'
 
-year = 2021
-month = 5
-first_date = Date.new(year, month, 1)
-last_date = Date.new(year, month, -1)
+# コマンド入力から年月を指定
+params = ARGV.getopts("y:", "m:")
+today = Date.today
+year = params["y"]
+month = params["m"]
+if year == nil
+  year = today.year
+elsif year.to_i < 1 || year.to_i > 9999
+  puts "cal: year `#{year}' not in range 1..9999"
+  return
+else
+  year = year.to_i
+end
+if month == nil
+  month = today.month
+elsif month.to_i < 1 || month.to_i > 12
+  puts "calcal: #{month} is neither a month number (1..12) nor a name"
+  return
+else
+  month = month.to_i
+end
 # カレンダーの装飾を追加
 str = "#{month}月 #{year}"
 puts str.center(20)
 puts "日 月 火 水 木 金 土"
 # 今月のカレンダーを表示
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
 (first_date.mday..last_date.mday).each do |i|
   date = Date.new(year, month, i)
   day = date.strftime('%e')

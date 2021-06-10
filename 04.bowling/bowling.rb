fa4e23b5f1252d03dd984fabbd66724173b42f9a
niikz <i.k307112@gmail.com>
2021-06-10 11:48:57 +0900

    フレームから合計スコアを計算して出力

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -26,3 +26,29 @@ shots.each do |s|
     end
   end
 end
+
+point = 0
+frames.each_with_index do |(first_throw, second_throw, third_throw), i|
+  frame_sum = first_throw.to_i + second_throw.to_i + third_throw.to_i
+  next_frame = frames[i + 1]
+  two_ahead_frame = frames[i + 2]
+  if i <= 8
+    next_frame_first = next_frame[0].to_i
+    next_frame_second = next_frame[1].to_i
+    if first_throw == 10 # strike
+      if i <= 7 && next_frame_first == 10
+        two_ahead_frame_first = two_ahead_frame[0].to_i
+        point += frame_sum + next_frame_first + two_ahead_frame_first
+      else
+        point += frame_sum + next_frame_first + next_frame_second
+      end
+    elsif frame_sum == 10 # spare
+      point += frame_sum + next_frame_first
+    else
+      point += frame_sum
+    end
+  elsif i > 8
+    point += frame_sum
+  end
+end
+puts point

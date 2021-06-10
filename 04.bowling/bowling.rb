b7d01d746427f4931aa700ff828a6dd69a9de83b
niikz <i.k307112@gmail.com>
2021-06-10 11:48:14 +0900

    配列をフレームに変換

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -10,3 +10,19 @@ scores.each do |s|
     shots << s.to_i
   end
 end
+
+frames = []
+frame = []
+shots.each do |s|
+  frame << s
+  if frames.size < 9 && (frame.size == 2 || (frame.size == 1 && s == 10))
+    frames << frame
+    frame = []
+  elsif frames.size >= 9
+    if frame.sum >= 10 && frame.size == 3
+      frames << frame
+    elsif frame.sum < 10 && frame.size == 2
+      frames << frame
+    end
+  end
+end

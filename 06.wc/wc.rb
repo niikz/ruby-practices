45887c12c645ea40ab39d0d34a2a19bee969b106
niikz <i.k307112@gmail.com>
2021-06-30 01:08:45 +0900

    -lオプションを実装

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,16 +1,22 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def count_file_data
+require 'optparse'
+
+option = ARGV.getopts('l')
+
+def count_file_data(option)
   ARGV.each do |file_name|
     text = File.read(file_name)
     print text.lines.count.to_s.rjust(8)
-    print text.split(/\s+|　+/).count.to_s.rjust(8)
-    print text.size.to_s.rjust(8)
+    unless option['l']
+      print text.split(/\s+|　+/).count.to_s.rjust(8)
+      print text.size.to_s.rjust(8)
+    end
     puts " #{file_name}"
   end
 end
 
 if ARGV[0]
-  count_file_data
+  count_file_data(option)
 end

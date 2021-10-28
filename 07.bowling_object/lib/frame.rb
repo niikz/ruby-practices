f59223caf405cc2c14f914040b3d10c5839a925b
niikz <i.k307112@gmail.com>
2021-10-28 15:47:25 +0900

    ストライク・スペアの判定を追加

--- 07.bowling_object/lib/frame.rb
+++ 07.bowling_object/lib/frame.rb
@@ -6,7 +6,7 @@ require_relative 'shot'
 class Frame
   attr_reader :first_shot, :second_shot, :third_shot
 
-  def initialize(first_mark, second_mark, third_mark = nil)
+  def initialize(first_mark, second_mark = nil, third_mark = nil)
     @first_shot = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark)
     @third_shot = Shot.new(third_mark)
@@ -15,10 +15,12 @@ class Frame
   def score
     [first_shot.score, second_shot.score, third_shot.score].sum
   end
+
+  def strike?
+    first_shot.score == 10
+  end
+
+  def spare?
+    !strike? && [first_shot.score, second_shot.score].sum == 10
+  end
 end
-
-frame_1 = Frame.new('1', '9', '8')
-p frame_1.score
-
-frame_2 = Frame.new('5', '2')
-p frame_2.score
--- 07.bowling_object/test/frame_test.rb
+++ 07.bowling_object/test/frame_test.rb
@@ -11,4 +11,20 @@ class FrameTest < Minitest::Test
     frame_2 = Frame.new('5', '2')
     assert_equal 7, frame_2.score
   end
+
+  def test_strike
+    frame_1 = Frame.new('X')
+    assert_equal 10, frame_1.score
+    assert frame_1.strike?
+  end
+
+  def test_spare
+    frame_1 = Frame.new('1', '9')
+    assert_equal 10, frame_1.score
+    assert frame_1.spare?
+
+    frame_2 = Frame.new('0', 'X')
+    assert_equal 10, frame_2.score
+    assert frame_2.spare?
+  end
 end

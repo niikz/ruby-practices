21042b2ba31d9825a34dbeae1dabd061e35aec42
niikz <i.k307112@gmail.com>
2021-11-10 16:56:18 +0900

    合計スコアのテスト

--- 07.bowling_object/test/game_test.rb
+++ 07.bowling_object/test/game_test.rb
@@ -4,17 +4,28 @@ require 'minitest/autorun'
 require_relative '../lib/game'
 
 class GameTest < Minitest::Test
-  def test_game_1
-    game_1 = Game.new('1,8,2,7,3,6,4,5,0,9,1,8,2,7,3,6,4,5,0,9')
-    assert_equal ["1", "8", "2", "7", "3", "6", "4", "5", "0", "9", "1", "8", "2", "7", "3", "6", "4", "5", "0", "9"], game_1.convert_to_shots
-    assert_equal [["1", "8"], ["2", "7"], ["3", "6"], ["4", "5"], ["0", "9"], ["1", "8"], ["2", "7"], ["3", "6"], ["4", "5"], ["0", "9"]], game_1.divide_into_frames
-    assert_equal 90, game_1.frame_score
+  def test_calc_1
+    game_1 = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
+    assert_equal 139, game_1.calc_score
   end
 
-  def test_game_2
-    game_2 = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
-    assert_equal ["6", "3", "9", "0", "0", "3", "8", "2", "7", "3", "X", "9", "1", "8", "0", "X", "6", "4", "5"], game_2.convert_to_shots
-    assert_equal [["6", "3"], ["9", "0"], ["0", "3"], ["8", "2"], ["7", "3"], ["X"], ["9", "1"], ["8", "0"], ["X"], ["6", "4", "5"]], game_2.divide_into_frames
-    assert_equal 94, game_2.frame_score
+  def test_calc_2
+    game_1 = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
+    assert_equal 164, game_1.calc_score
+  end
+
+  def test_calc_3
+    game_1 = Game.new('0,X,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
+    assert_equal 107, game_1.calc_score
+  end
+
+  def test_calc_all_zero
+    game_1 = Game.new('0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+    assert_equal 0, game_1.calc_score
+  end
+
+  def test_calc_perfect
+    game_1 = Game.new('X,X,X,X,X,X,X,X,X,X,X,X')
+    assert_equal 300, game_1.calc_score
   end
 end
